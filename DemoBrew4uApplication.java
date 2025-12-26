cfc4c184080ebda042d9d5412eba4bca2abafdf5
alexandrayohana <as8019707@gmail.com>
2025-12-26 17:46:00 +0700

    Create DemoBrew4uApplication.java

---
+++ DemoBrew4uApplication.java
@@ -0,0 +1,13 @@
+package com.impal.demo_brew4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBrew4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBrew4uApplication.class, args);
+	}
+
+}
